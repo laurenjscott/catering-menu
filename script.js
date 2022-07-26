d271d9dc5bb83f17c11b9e449f1ace5702c69c7c
Lauren Scott <82675662+laurenjscott@users.noreply.github.com>
2022-07-26 15:23:49 -0500

    RW-4 Added function to open dialog

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
+/*Elements**************/
+const main = document.querySelector("main");
+const dialog = document.querySelector("dialog");
+const dialogPara = document.querySelector("dialog p");
+
 /*Data**************/
 const json =
   '{ "menu" : { "Appetizers" : { "generalDescription" : "Minimum 2 dozen", "menuItems" : [ { "glutenFree" : false, "itemName" : "Mini Crab Cakes", "perDozen" : true, "perServing" : false, "price" : 34.99 }, { "glutenFree" : true, "itemName" : "Beef Tenderloin Skewers w/Chimichurri Sauce", "perDozen" : true, "perServing" : false, "price" : 29.99 }, { "glutenFree" : true, "itemName" : "Jumbo Shrimp Cocktail", "perDozen" : true, "perServing" : false, "price" : 29.99 }, { "glutenFree" : false, "itemName" : "Mini Onion Tartlets", "perDozen" : true, "perServing" : false, "price" : 19.99 }, { "glutenFree" : false, "itemName" : "Spinach Tartlets", "perDozen" : true, "perServing" : false, "price" : 19.99 }, { "glutenFree" : true, "itemName" : "Maple Glazed Bacon Wrapped Chicken", "perDozen" : true, "perServing" : false, "price" : 19.99 }, { "glutenFree" : true, "itemName" : "Bacon Wrapped Dates", "perDozen" : true, "perServing" : false, "price" : 19.99 }, { "glutenFree" : false, "itemName" : "Locally Sourced Cheese Board", "perDozen" : false, "perServing" : true, "price" : 7.99 }, { "glutenFree" : false, "itemName" : "Vegetable Crudite Platter", "perDozen" : false, "perServing" : true, "price" : 4.99 } ] }, "Soups & Salads" : { "generalDescription" : "Prices per serving", "menuItems" : [ { "glutenFree" : true, "itemName" : "Winter Apple Kale Salad", "price" : 4.99 }, { "glutenFree" : false, "itemName" : "Spinach and Bacon Salad", "price" : 4.99 }, { "glutenFree" : true, "itemName" : "Raspberry Vinaigrette Salad", "price" : 3.99 }, { "glutenFree" : true, "itemName" : "Butternut Squash Soup", "price" : 3.99 }, { "glutenFree" : false, "itemName" : "Chicken and Wild Rice Soup", "price" : 3.99 } ] }, "Entrees" : { "generalDescription" : "Served with fresh baked rolls and whipped butter. Prices per serving", "menuItems" : [ { "glutenFree" : false, "itemName" : "Jumbo Lump Crab Cakes with Remoulade Sauce", "price" : 16.99 }, { "glutenFree" : true, "itemName" : "USDA Choice Filet Mignon w/ Port Reduction", "price" : 14.99 }, { "glutenFree" : true, "itemName" : "Herb Grilled Salmon w/ Fresh Dill Sauce", "price" : 14.99 }, { "glutenFree" : false, "itemName" : "Lobster Ravioli", "price" : 14.99 }, { "glutenFree" : true, "itemName" : "Spiral Cut Ham w/ Maple Honey Glaze", "price" : 12.99 }, { "glutenFree" : false, "itemName" : "Smoked Turkey w/ Homemade Gravy", "price" : 12.99 }, { "glutenFree" : false, "itemName" : "Butternut Squash Ravioli w/ Sage Cream Sauce ", "price" : 12.99 } ] }, "Sides" : { "generalDescription" : "All sides $3.99 per serving", "menuItems" : [ { "glutenFree" : true, "itemName" : "Wild Rice Pilaf w/ Cranberries and Almonds" }, { "glutenFree" : true, "itemName" : "Winter Root Vegetable Medley" }, { "glutenFree" : true, "itemName" : "Green Bean Casserole w/ Frizzled Onions" }, { "glutenFree" : false, "itemName" : "Baked Four Cheese Macaroni" }, { "glutenFree" : true, "itemName" : "Cheddar Mashed Potatoes" }, { "glutenFree" : false, "itemName" : "Sourdough Stuffing" }, { "glutenFree" : true, "itemName" : "Pecan Crusted Mashed Sweet Potatoes" } ] } } }';
 
 const menuObj = JSON.parse(json);
 
-const main = document.querySelector("main");
+/****************************************************/
+
 
 renderFullMenu();
+displayGFDialog();
 
 /*Functions*************************/
 function renderFullMenu() {
@@ -44,4 +51,21 @@ function renderMenuItems(key, article) {
 		}</span>`;
     ul.appendChild(li);
   });
+}
+
+function displayGFDialog() {
+    const glutenFreeMenuItems = [];
+        Object.keys(menuObj.menu).forEach(
+            key => {menuObj.menu[key].menuItems.forEach(
+                menuItem => {
+                    if(menuItem.glutenFree === true) {
+                        glutenFreeMenuItems.push(menuItem.itemName);
+                }
+            }
+        )
+    });
+    if(glutenFreeMenuItems.length > 0) {
+       dialog.showModal();
+       dialogPara.innerHTML = `<span class="bold dialog-title">Gluten Free?</span><br><span class="bold">Save 25%</span> on your ${glutenFreeMenuItems[Math.floor((glutenFreeMenuItems.length - 1) * Math.random())]} purchase today, ${new Intl.DateTimeFormat("en-us").format(new Date())}.`;
+    }
 }

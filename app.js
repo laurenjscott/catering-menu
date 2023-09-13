95f4f165b921f4cba048edd13d9c98a6ba4a0dbd
Lauren Scott <82675662+laurenjscott@users.noreply.github.com>
2023-09-13 11:50:13 -0500

    Added function to create main nav links

--- app.js
+++ app.js
@@ -1,6 +1,44 @@
+window.addEventListener("load", event => {
+    const hamburger = document.querySelector("header > button:first-of-type");
+    hamburger.addEventListener("click", showMainNavigation);
+    modifyCopyrightYear();
+});
+
+
 function modifyCopyrightYear() {
     let copyright = document.querySelector("footer > p");
     copyright.textContent = copyright.textContent.replace(/\d{4}/, new Date().getFullYear());
 }
 
-modifyCopyrightYear();
+
+export default function populateMainNavigation(folder) { //argument is temporary
+    //fileName is temporarily inactive while host via local host. "if" condition will use fileName once in production
+//    const fileName = document.querySelector("html").baseURI.split("/").pop();
+    const navUl = document.querySelector("header nav ul");
+    if(folder == "order-form") {
+        //populate nav with links to index.html and current-menu.html
+        const homeListItem = document.createElement("li");
+        const currentMenuListItem = document.createElement("li");
+        const homeAnchor = document.createElement("a");
+        const currentMenuAnchor = document.createElement("a");
+        homeAnchor.href = "../index.html";
+        homeAnchor.textContent = "Home";
+        currentMenuAnchor.href = "../current-menu/menu.html";
+        currentMenuAnchor.textContent = "Current Menu";
+        homeListItem.appendChild(homeAnchor);
+        currentMenuListItem.appendChild(currentMenuAnchor);
+        navUl.appendChild(homeListItem);
+        navUl.appendChild(currentMenuListItem);
+
+    }
+}
+
+function showMainNavigation() {
+    const nav = document.querySelector("header nav");
+    nav.classList.add("show-main-nav");
+}
+
+
+
+
+

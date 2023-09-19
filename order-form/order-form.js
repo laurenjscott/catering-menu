6cea106dfd862f5b485f4f8ac33a7d0b2c8d4209
Lauren Scott <82675662+laurenjscott@users.noreply.github.com>
2023-09-19 18:12:38 -0500

    Implemented debouncer workflow for event date input
    Also, wrote functions to enable and disable menu item links based on if event time and date input values are valid.

--- order-form/order-form.js
+++ order-form/order-form.js
@@ -1,9 +1,14 @@
-import populateMainNavigation from "../app.js"; //Why is this being imported instead of being run in app.js? Becauseof the issue with Dreamweaver live server not displaying the true URL of the HTML file it's called on. Once this is in prod, it can be run in app.js.
+import populateMainNavigation from "../app.js"; //Why is this being imported instead of being run in app.js? Because of the issue with Dreamweaver live server not displaying the true URL of the HTML file it's called on. Once this is in prod, it can be run in app.js.
+
 
 window.addEventListener("load", (event) => {
-    
+        
+    const cartButton = document.querySelector("header > button:last-of-type");
+    cartButton.addEventListener("click", showCart);
+
     const eventDateInput = document.querySelector("#event-date-time-picker-section input");
-    eventDateInput.addEventListener("change", (event) => validateEventDate(event));
+//    eventDateInput.addEventListener("change", (event) => validateEventDate(event));
+    eventDateInput.addEventListener("change", (event) => processChange(event)); //When user enters a date into the event date input, processChange() is called and is passed the "change" event. processChange() basically calls (or invokes) the function declaration returned from debounce().
     
     const eventTimeSelect = document.querySelector("#event-date-time-picker-section select");
     eventTimeSelect.addEventListener("change", (event) => validateEventTime(event));
@@ -15,9 +20,7 @@ window.addEventListener("load", (event) => {
     });
     
     
-	const addToCartButton = document.querySelector(
-		"#quantity-section #add-to-cart-button"
-	);
+	const addToCartButton = document.querySelector("#quantity-section #add-to-cart-button");
 	addToCartButton.addEventListener("click", (event) => {
 		//grab menu item's label text content
 		const menuItemName = document.querySelector("label:first-of-type")
@@ -68,9 +71,7 @@ window.addEventListener("load", (event) => {
 
 	});
 
-	const decreaseQuantityButton = document.querySelector(
-		"#decrease-quantity-button"
-	);
+	const decreaseQuantityButton = document.querySelector("#decrease-quantity-button");
 	decreaseQuantityButton.addEventListener("click", (event) => {
 		const numberInput = document.querySelector("input[type='number']");
 		if (numberInput.value > 1) {
@@ -104,18 +105,52 @@ window.addEventListener("load", (event) => {
     
     //additional of argument is temporary while hosted via localhost
     populateMainNavigation(import.meta.url.split("/").pop().split(".")[0]);
+    
 
 });
 
 
+function toggleMenuItemLinks(boolean) { //links are disabled if there is no event date and time data available. links are enabled if there is both event date and time data available
+    const allMenuItemLinks = [...document.querySelectorAll("#menu-items a")];
+    if(boolean === true) {
+        allMenuItemLinks.forEach(a => {
+            a.classList.remove("disabled-menu-item-link");
+        })
+    } else {
+        allMenuItemLinks.forEach(a => {
+            a.classList.add("disabled-menu-item-link");
+        })
+    }
+   
+}
+
+function disableMenuItemLinks() { //links are enabled if there is both event date and time data available
+    const allMenuItemLinks = [...document.querySelectorAll("#menu-items a")];
+    allMenuItemLinks.forEach(a => {
+        a.classList.add("disabled-menu-item-link");
+    })
+}
+
 
 function updateCartCount() {
+    const cartButton = document.querySelector("header > button:last-of-type");
     const cart = sessionStorage.cart;
-    const cartPara = document.querySelector("#cart-wrapper p");
+    const cartSpanText = document.querySelector("header > button:last-of-type span:last-of-type");
     if(cart == undefined) {
-        cartPara.textContent = "0";
+        cartSpanText.textContent = "0";
+        cartButton.setAttribute("disabled", true);
+        
     } else {
-         cartPara.textContent = `${JSON.parse(cart).length}`;
+        cartSpanText.textContent = `${JSON.parse(cart).length}`;
+        cartButton.removeAttribute("disabled");
+
+
+    }
+}
+
+function showCart() {
+    if(sessionStorage.cart != undefined) {
+        console.log(sessionStorage.cart);
     }
 }
 
@@ -130,7 +165,7 @@ function updateSubtotal(num) {
     }).format(num * pricePerUnit);
 }
 
-function pushEventDateRangeToInputs() {
+function pushEventDateRangeToInputs() { 
     const now = new Date();
     const offset = now.getTimezoneOffset(); // minutes between Chicago and London time
     const todayOffset = new Date(now.getTime() - (offset*60*1000)); // Since toISOString() method uses UTC, I must apply an additional offset to the original todaysDate variable to preserve UTC-5 offset. See https://stackoverflow.com/questions/23593052/format-javascript-date-as-yyyy-mm-
@@ -145,36 +180,85 @@ function pushEventDateRangeToInputs() {
     dateInput.setAttribute("max", maxDateValue);      
 }
 
+
 function validateEventDate(event) {
     const dateInput = event.target;
-    const minMaxInvalidString = document.querySelector("#event-date-time-picker-section p span");
-    if(dateInput.validity.rangeUnderflow === true || dateInput.validity.rangeOverflow === true) {
+    const minMaxInvalidString = document.querySelector(
+        "#event-date-time-picker-section p span"
+    );
+    if (
+        dateInput.validity.rangeUnderflow === true ||
+        dateInput.validity.rangeOverflow === true
+    ) {
         //target descriptive text
-        minMaxInvalidString.classList.add("invalidMinMaxDescription");
+        minMaxInvalidString.classList.add("invalid-min-max-description");
         //report validity
         dateInput.reportValidity();
-        //make text red
-        
-    } else if(dateInput.validity.valid === false) {
+        //disable all menu item links
+        toggleMenuItemLinks(false);
+    } else if (dateInput.validity.valid === false) {
         //un-target descriptive text
-        minMaxInvalidString.classList.remove("invalidMinMaxDescription");
-        //report validity
+        minMaxInvalidString.classList.remove("invalid-min-max-description");
+//        //report validity
         dateInput.reportValidity();
-        //make text red
-        
+        //disable all menu item links
+        toggleMenuItemLinks(false);
+
     } else {
         //un-target descriptive text
-        minMaxInvalidString.classList.remove("invalidMinMaxDescription");
-       
+        minMaxInvalidString.classList.remove("invalid-min-max-description");
+        //Do both event time/date inputs have valid values? If so, enable all menu item links
+        checkFullEventInfoValidation();
     }
+    
 }
+
 
 function validateEventTime(event) {
     const timeInput = event.target;
     if(timeInput.validity.valid === false) {
         //report validity
         timeInput.reportValidity();
+        //disable all menu item links
+         toggleMenuItemLinks(false);
         
+    } else {
+        //Do both event time/date inputs have valid values? If so, enable all menu item links
+        checkFullEventInfoValidation();
     }
 }
+
+function checkFullEventInfoValidation() {
+    const allEventTimeDateInputs = [...document.querySelectorAll("#event-date-time-picker-section input, #event-date-time-picker-section select")];
+    if(allEventTimeDateInputs.every(input => input.validity.valid === true)) {
+        toggleMenuItemLinks(true);
+        //add event listener to links
+        const menuItemLinks = [...document.querySelectorAll("#menu-items a")];
+        menuItemLinks.forEach(a => {
+            a.addEventListener("click", () => document.querySelector("dialog").showModal());
+        })
+    } else {
+        //disable menu item links
+        toggleMenuItemLinks(false);
+    }
+}
+
+
+
+
+
+function debounce(func, timeout = 500){ //Used to allow user to complete data entry before checking for validation errors.
+    let timer; 
+    function test(...args) {
+        clearTimeout(timer);
+        timer = setTimeout(() => func(...args), timeout);
+    };
+    return test;
+}
+
+const processChange = debounce((event) => validateEventDate(event)); //processChange is assigned the return value from debounce(), which is a function declaration. When processChange(event) is called via event trigger on the date input, the following anonymous function is invoked:
+//processChange = (...args) => { // the "change event" is the argument passed to this function
+//        clearTimeout(timer); //does nothing if timer is undefined. Else, cancels previous setTimeout call. 
+//        timer = setTimeout(() => { func.apply(this, args)}, timeout); // After 500 ms (0.5 sec), runs function passed to debounce(), which is "(event) => validateEventDate(event)". The timer variable is then set with integer. From MDN's setTimeout docs: "The returned timeoutID is a positive integer value which identifies the timer created by the call to setTimeout(). This value can be passed to clearTimeout() to cancel the timeout."
+//    }
 

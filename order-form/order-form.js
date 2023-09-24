31525ea61a29ac0c3aefc96b4b7705632367207f
Lauren Scott <82675662+laurenjscott@users.noreply.github.com>
2023-09-24 12:44:38 -0500

    Fetch menu JSON
    Wrote functions to fetch current menu and render it on-screen. Reorganized so that window load event doesn't bind non-essential listener function if fetch failed.

--- order-form/order-form.js
+++ order-form/order-form.js
@@ -1,77 +1,69 @@
 import populateMainNavigation from "../app.js"; //Why is this being imported instead of being run in app.js? Because of the issue with Dreamweaver live server not displaying the true URL of the HTML file it's called on. Once this is in prod, it can be run in app.js.
 
 
-window.addEventListener("load", (event) => {
-        
+window.addEventListener("load", () => {
+    //additional of argument is temporary while hosted via localhost
+    populateMainNavigation(import.meta.url.split("/").pop().split(".")[0]); 
+    
+    updateShowCartButtonString();
+    
+    // The following event listener is being bound to the cart button outside of bindEventListeners() because it needs to be invoked even if the fetching of data fails. The user should still have access to their cart items, right?
     const cartButton = document.querySelector("header > button:last-of-type");
     cartButton.addEventListener("click", showCart);
 
-    const eventDateInput = document.querySelector("#event-date-time-picker-section input");
-//    eventDateInput.addEventListener("change", (event) => validateEventDate(event));
-    eventDateInput.addEventListener("change", (event) => processChange(event)); //When user enters a date into the event date input, processChange() is called and is passed the "change" event. processChange() basically calls (or invokes) the function declaration returned from debounce().
-    
-    const eventTimeSelect = document.querySelector("#event-date-time-picker-section select");
-    eventTimeSelect.addEventListener("change", (event) => validateEventTime(event));
-    
-    const closeOrderFormDialogButton = document.querySelector("dialog > button");
-    closeOrderFormDialogButton.addEventListener("click", event => {
-        const dialog = document.querySelector("dialog");
-        dialog.close();
-    });
-    
-    
-	const addToCartButton = document.querySelector("#quantity-section #add-to-cart-button");
-	addToCartButton.addEventListener("click", (event) => {
-		//grab menu item's label text content
-		const menuItemName = document.querySelector("dialog label:first-of-type")
-			.textContent;
+    fetchCurrentMenu().then((data) => {
+        bindEventListeners();
+        pushEventDateRangeToInputs();
+        //render menu
+        renderMenu(data);
+        
+        
+    }).catch((error) => {
+        console.error(error)
+       //hide event date/time section
+        const eventDataTimeSection = document.querySelector("#event-date-time-picker-section");
+        eventDataTimeSection.classList.add("hide-element");
+        document.querySelector("main > hr:first-of-type").classList.add("hide-element");
+        //Disable inputs
+        const allVisibleEventTimeDateInputs = [...document.querySelectorAll("#event-date-time-picker-section input:not([hidden]), #event-date-time-picker-section select:not([hidden])")];
+        allVisibleEventTimeDateInputs.forEach(input => {
+            input.setAttribute("disabled", true);
+        });
+        //Report data load fail to user
+        const fetchFailPara = document.querySelector("#menu-items > p");
+        fetchFailPara.classList.remove("hide-element");
+    })
+});
 
-		//grab menu item description
-		const description = document.querySelector("dialog label:first-of-type + p")
-			.textContent;
+function bindEventListeners() {
+	const eventDateInput = document.querySelector(
+		"#event-date-time-picker-section input"
+	);
+	eventDateInput.addEventListener("change", (event) => processChange(event)); //When user enters a date into the event date input, processChange() is called and is passed the "change" event. processChange() basically calls (or invokes) the function declaration returned from debounce().
 
-		//grab qty requested
-		const qty = parseInt(document.querySelector("dialog input[type='number']").value);
+	const eventTimeSelect = document.querySelector(
+		"#event-date-time-picker-section select"
+	);
+	eventTimeSelect.addEventListener("change", (event) =>
+		validateEventTime(event)
+	);
 
-		//grab special instructions
-		const specialInstructions = document.querySelector("dialog textarea").value;
-
-		//grab price per unit
-		const pricePerUnit = parseInt(
-			document.querySelector("dialog input[type='hidden']").value
-		);
-
-		//calculate subtotal
-		const subtotal = parseInt(
-			document.querySelector("dialog output").textContent.replace(/\$/g, "")
-		);
-
-		//form a object based off form information
-		const orderLineItemObj = {};
-		orderLineItemObj.menuItemName = menuItemName;
-		orderLineItemObj.description = description;
-		orderLineItemObj.specialInstructions = specialInstructions;
-		orderLineItemObj.qty = qty;
-		orderLineItemObj.pricePerUnit = pricePerUnit;
-		orderLineItemObj.subtotal = subtotal;
-
-		//determine if cart is empty. If it is, create it in session storage. If it's not, grab current cart, append new line item, and add back to session storage
-		const cart = sessionStorage.getItem("cart");
-		if (cart == null) {
-			//cart is empty
-			const cartArray = [];
-			cartArray.push(orderLineItemObj);
-			sessionStorage.setItem("cart", JSON.stringify(cartArray));
-		} else {
-			const previousCartItemsArray = JSON.parse(sessionStorage.getItem("cart"));
-			previousCartItemsArray.push(orderLineItemObj);
-			sessionStorage.setItem("cart", JSON.stringify(previousCartItemsArray));
-		}
-        updateCartCount();
-
+	const closeOrderFormDialogButton = document.querySelector("dialog > button");
+	closeOrderFormDialogButton.addEventListener("click", (event) => {
+		const dialog = document.querySelector("dialog");
+		dialog.close();
 	});
 
-	const decreaseQuantityButton = document.querySelector("#decrease-quantity-button");
+	const addToCartButton = document.querySelector(
+		"#quantity-section #add-to-cart-button"
+	);
+	addToCartButton.addEventListener("click", (event) => {
+		addToCart(event);
+	});
+
+	const decreaseQuantityButton = document.querySelector(
+		"#decrease-quantity-button"
+	);
 	decreaseQuantityButton.addEventListener("click", (event) => {
 		const numberInput = document.querySelector("input[type='number']");
 		if (numberInput.value > 1) {
@@ -94,17 +86,59 @@ window.addEventListener("load", (event) => {
 			decreaseQuantityButton.removeAttribute("disabled");
 		}
 	});
+}
 
-    
-    updateCartCount();
-    
-    pushEventDateRangeToInputs();
-    
-    //additional of argument is temporary while hosted via localhost
-    populateMainNavigation(import.meta.url.split("/").pop().split(".")[0]);    
 
-});
+function addToCart(event) {
+    //grab menu item's label text content
+		const menuItemName = document.querySelector("dialog label:first-of-type")
+			.textContent;
 
+		//grab menu item description
+		const description = document.querySelector("dialog label:first-of-type + p")
+			.textContent;
+
+		//grab qty requested
+		const qty = parseInt(
+			document.querySelector("dialog input[type='number']").value
+		);
+
+		//grab special instructions
+		const specialInstructions = document.querySelector("dialog textarea").value;
+
+		//grab price per unit
+		const pricePerUnit = parseInt(
+			document.querySelector("dialog input[type='hidden']").value
+		);
+
+		//calculate subtotal
+		const subtotal = parseInt(
+			document.querySelector("dialog output").textContent.replace(/\$/g, "")
+		);
+
+		//form a object based off of form information
+		const orderLineItemObj = {};
+		orderLineItemObj.menuItemName = menuItemName;
+		orderLineItemObj.description = description;
+		orderLineItemObj.specialInstructions = specialInstructions;
+		orderLineItemObj.qty = qty;
+		orderLineItemObj.pricePerUnit = pricePerUnit;
+		orderLineItemObj.subtotal = subtotal;
+
+		//determine if cart is empty. If it is, create it in session storage. If it's not, grab current cart, append new line item, and add back to session storage
+		const cart = sessionStorage.getItem("cart");
+		if (cart == null) {
+			//cart is empty
+			const cartArray = [];
+			cartArray.push(orderLineItemObj);
+			sessionStorage.setItem("cart", JSON.stringify(cartArray));
+		} else {
+			const previousCartItemsArray = JSON.parse(sessionStorage.getItem("cart"));
+			previousCartItemsArray.push(orderLineItemObj);
+			sessionStorage.setItem("cart", JSON.stringify(previousCartItemsArray));
+		}
+		updateShowCartButtonString();
+}
 
 function toggleMenuItemLinks(boolean) { //links are disabled if there is no event date and time data available. links are enabled if there is both event date and time data available
     const allMenuItemLinks = [...document.querySelectorAll("#menu-items a")];
@@ -120,8 +154,7 @@ function toggleMenuItemLinks(boolean) { //links are disabled if there is no even
    
 }
 
-
-function updateCartCount() {
+function updateShowCartButtonString() {
     const cartButton = document.querySelector("header > button:last-of-type");
     const cart = sessionStorage.cart;
     const cartSpanText = document.querySelector("header > button:last-of-type span:last-of-type");
@@ -135,6 +168,86 @@ function updateCartCount() {
 
 
     }
+}
+
+
+async function fetchCurrentMenu() {
+    const response = await fetch("../current-menu/nf-catering-menu.json");
+    if(!response.ok) {
+        throw new Error(`Cannot fetch menu object.`); //this is similar to a return
+    }
+    //render menu
+    const json = await response.json();
+    return json;
+}
+
+function renderMenu(obj) {
+        Object.keys(obj.menu).forEach((key) => { //loop through all categories in the menu object. "key" is the individual menu category. obj is the menu object
+        const menuSection = document.querySelector("#menu-items");
+        //create an article and append it to the menu section
+        let article = document.createElement("article");
+        menuSection.appendChild(article);
+        //Add a heading
+        let h3 = document.createElement("h3");
+        h3.textContent = key;
+        article.appendChild(h3);
+        //Add a subheading
+        let p = document.createElement("p");
+        p.textContent = obj.menu[key].generalDescription;
+        article.appendChild(p);
+        renderMenuItems(obj, key, article);//sub-function that adds menu items for the current category
+    });
+}
+
+function renderMenuItems(obj, key, article) {// sub-function of renderMenu() function. Loops thru each of the categories' individual menu items. "obj" is the menu object, key" is the category's name. and "article" is the HTML article element that represents the category in the DOM.
+  let ul = document.createElement("ul");
+  article.appendChild(ul);
+  obj.menu[key].menuItems.forEach((item) => {
+    let li = document.createElement("li");
+    let a = document.createElement("a");
+    let img = document.createElement("img");
+    let h4 = document.createElement("h4");
+    let imageWrapper = document.createElement("div");
+    let textWrapper = document.createElement("div");
+    let pricePara = document.createElement("p");
+    let menuItemDescription = document.createElement("p");
+    a.appendChild(imageWrapper);
+    a.appendChild(textWrapper);
+    imageWrapper.appendChild(img);
+    textWrapper.appendChild(h4);
+    textWrapper.appendChild(pricePara);
+    ul.appendChild(li);
+    li.appendChild(a);
+    li.dataset.price = item.price;
+    li.dataset.menuItemName = item.itemName;
+    li.dataset.glutenFree = item.glutenFree;
+    li.dataset.perServing = item.perServing;
+    li.dataset.perUnit = item.perUnit;
+    li.dataset.perDozen = item.perDozen;
+    a.setAttribute("href", "#menu-item-dialog");
+    a.classList.add("disabled-menu-item-link");
+    imageWrapper.classList.add("image-wrapper");
+    img.setAttribute("src", "../assets/nadine-primeau-l5Mjl9qH8VU-unsplash.jpg");
+    img.setAttribute("alt", "image of food")
+    img.setAttribute("width", "100");
+    img.setAttribute("aria-hidden", true);
+    textWrapper.classList.add("text-wrapper");
+    h4.textContent = item.itemName;
+    if(item.glutenFree) {
+        let glutenFreeSpan = document.createElement("span");
+        glutenFreeSpan.textContent = "GF";
+        glutenFreeSpan.classList.add("gluten-free-tag");
+        glutenFreeSpan.setAttribute("aria-label", "Gluten Free");
+        glutenFreeSpan.setAttribute("title", "This item is gluten free.");
+        h4.prepend(glutenFreeSpan);
+    };
+    pricePara.textContent = `${new Intl.NumberFormat("en-US", { style: "currency", currency: "USD" }).format(item.price)} ${item.perDozen ? "per dozen" : item.perUnit ? "per unit" : item.perServing ? "per serving" : ""}`;
+    if(item.description != undefined) {
+        textWrapper.appendChild(menuItemDescription);
+        menuItemDescription.textContent = item.description;
+    } 
+    ul.appendChild(li);
+  });
 }
 
 function emptyCart() {
@@ -180,7 +293,6 @@ function pushEventDateRangeToInputs() {
     dateInput.setAttribute("max", maxDateValue);      
 }
 
-
 function validateEventDate(event) {
     const dateInput = event.target;
     const minMaxInvalidString = document.querySelector(
@@ -209,7 +321,6 @@ function validateEventDate(event) {
         checkFullEventInfoValidation();
     } 
 }
-
 
 function validateEventTime(event) {
     const timeInput = event.target;
@@ -312,4 +423,5 @@ const processChange = debounce((event) => validateEventDate(event)); //processCh
 //        clearTimeout(timer); //does nothing if timer is undefined. Else, cancels previous setTimeout call. 
 //        timer = setTimeout(() => { func.apply(this, args)}, timeout); // After 500 ms (0.5 sec), runs function passed to debounce(), which is "(event) => validateEventDate(event)". The timer variable is then set with integer. From MDN's setTimeout docs: "The returned timeoutID is a positive integer value which identifies the timer created by the call to setTimeout(). This value can be passed to clearTimeout() to cancel the timeout."
 //    }
+
 

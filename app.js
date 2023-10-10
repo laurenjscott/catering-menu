fc9229b6b96e78515b7b9a7deadb138a1ef95ea3
Lauren Scott <82675662+laurenjscott@users.noreply.github.com>
2023-10-10 11:57:59 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,34 @@
+export default function populateMainNavigation(folder) { //folder argument is temporary
+    //fileName is temporarily inactive while host via local host. "if" condition will use fileName once in production
+//    const fileName = document.querySelector("html").baseURI.split("/").pop();
+    const navUl = document.querySelector("header nav ul");
+    const homeListItem = document.createElement("li");
+    const currentMenuListItem = document.createElement("li");
+    const orderFormListItem = document.createElement("li");
+    const homeAnchor = document.createElement("a");
+    const currentMenuAnchor = document.createElement("a");
+    const orderFormAnchor = document.createElement("a");
+    homeAnchor.href = "../index.html";
+    homeAnchor.textContent = "Home";
+    currentMenuAnchor.href = "../current-menu/menu.html";
+    currentMenuAnchor.textContent = "Current Menu";
+    orderFormAnchor.href = "../order-form/order-form.html";
+    orderFormAnchor.textContent = "Order Online";
+    homeListItem.appendChild(homeAnchor);
+    currentMenuListItem.appendChild(currentMenuAnchor);
+    orderFormListItem.appendChild(orderFormAnchor);
+    if(folder == "order-form") {
+        //populate nav with links to index.html and current-menu.html
+        navUl.appendChild(homeListItem);
+        navUl.appendChild(currentMenuListItem);
+    } else if(folder == "menu") {
+        //populate nav with links to index.html and order-form.html
+        navUl.appendChild(homeListItem);
+        navUl.appendChild(orderFormListItem);
+    }
+}
+
+
 window.addEventListener("load", event => {
     addListenersToMainNavButtons();
     modifyCopyrightYear();
@@ -14,28 +45,6 @@ function modifyCopyrightYear() {
     copyright.textContent = copyright.textContent.replace(/\d{4}/, new Date().getFullYear());
 }
 
-
-export default function populateMainNavigation(folder) { //folder argument is temporary
-    //fileName is temporarily inactive while host via local host. "if" condition will use fileName once in production
-//    const fileName = document.querySelector("html").baseURI.split("/").pop();
-    const navUl = document.querySelector("header nav ul");
-    if(folder == "order-form") {
-        //populate nav with links to index.html and current-menu.html
-        const homeListItem = document.createElement("li");
-        const currentMenuListItem = document.createElement("li");
-        const homeAnchor = document.createElement("a");
-        const currentMenuAnchor = document.createElement("a");
-        homeAnchor.href = "../index.html";
-        homeAnchor.textContent = "Home";
-        currentMenuAnchor.href = "../current-menu/menu.html";
-        currentMenuAnchor.textContent = "Current Menu";
-        homeListItem.appendChild(homeAnchor);
-        currentMenuListItem.appendChild(currentMenuAnchor);
-        navUl.appendChild(homeListItem);
-        navUl.appendChild(currentMenuListItem);
-
-    }
-}
 
 function addListenersToMainNavButtons() {
     const buttonArray = [...document.querySelectorAll("header button:first-of-type")];
